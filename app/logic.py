20cac20f2d8c596984d1f63cf62771befae18e5b
bhupesh8108 <bhupeshdawadi12345@gmail.com>
2023-05-05 15:15:03 +0545

    automatic update

--- app/logic.py
+++ app/logic.py
@@ -1,35 +1,9 @@
-from email.message import EmailMessage
-import smtplib , ssl, random
 from django.core.mail import send_mail
-# def send_email(request, product,user,email,quantity,address,phone_number):
-#     password = 'fokaauhqkeakxngp'
-#     sender = 'info.giftwan@gmail.com'
-#     # reveiver = ['binitshrestha832@gmail.com','poudelbishal69@gmail.com']
-#     reveiver = ['bhupeshdawadi12345@gmail.com']
-#     subject = 'New order recieved'
-#     body = f'''
-#     product : {product}
-#     quantity : {quantity}
-#     address : {address}
-#     phone_number : {phone_number}
-#     user_email : {email}
-#     user_name ; {user}
-#     '''
-#     print(body)
-#     em = EmailMessage()
-#     em['From'] = sender
-#     em['Subject'] = subject
-#     em['To'] = sender
-#     em.set_content(body)
-#     context = ssl.create_default_context()
-#     with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-#         smtp.login(sender,password)
-#         smtp.sendmail(sender,reveiver,em.as_string())
-
-
 def send_reset_link(token,email):
     subject = "Password reset link"
-    message = f"https://giftwan.pythonanywhere.com/password-set/{token}"
+    message = f"""Here is the link to reset you password Please follow 
+    instructions to reset your password. Thank you and keep gifting you loved ones with giftwan.pythonanywhere.com
+    https://giftwan.pythonanywhere.com/password-set/{token}"""
     sender = "info.giftwan@gmail.com"
     recipient = [email]
     send_mail(subject,message,sender,recipient)
