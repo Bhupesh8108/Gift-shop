5f48145178981947a8f58349628c8793a10b4d19
bhupesh8108 <bhupeshdawadi12345@gmail.com>
2023-05-07 16:37:03 +0545

    automatic update

--- app/logic.py
+++ app/logic.py
@@ -2,8 +2,8 @@ from django.core.mail import send_mail
 def send_reset_link(token,email):
     subject = "Password reset link"
     message = f"""Here is the link to reset you password Please follow 
-    instructions to reset your password. Thank you and keep gifting you loved ones with giftwan.pythonanywhere.com
-    http://127.0.0.1:8000//password-set/{token}"""
+    instructions to reset your password. Thank you and keep gifting you loved ones with giftwan
+   https://giftwan.pythonanywhere.com/password-set/{token}"""
     sender = "info.giftwan@gmail.com"
     recipient = [email]
     send_mail(subject,message,sender,recipient)
